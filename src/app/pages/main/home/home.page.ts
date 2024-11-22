463cbd09c0849049b862763a6b994fde63ed48e7
FileAlex <cesar.alejcontreras17@gmail.com>
2024-11-22 06:50:25 -0600

    headers

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,14 @@ import { Component, inject, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  
+  firebaseSvc= inject(FirebaseService);
+  utilsSvc= inject(UtilsService);
 
   ngOnInit() {
+  }
+
+  singOut(){
+    this.firebaseSvc.singOut;
   }
 
 
